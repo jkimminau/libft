5d980d76a17a44611d335217446c06967ca75121
Jamison Kimminau <thepistochini@gmail.com>
2018-02-26 00:23:24 -0800

    hm

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jkimmina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 11:06:16 by jkimmina          #+#    #+#             */
-/*   Updated: 2018/02/24 21:43:56 by jkimmina         ###   ########.fr       */
+/*   Updated: 2018/02/26 00:23:13 by jkimmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	return (NULL);
 	while (*s)
 	{
 		if (*s == c)
