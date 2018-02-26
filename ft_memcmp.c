dc67291fd4fd16c371f321ffff29f73ef8b62aee
Jamison Kimminau <thepistochini@gmail.com>
2018-02-26 00:31:35 -0800

    l

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jkimmina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 21:27:34 by jkimmina          #+#    #+#             */
-/*   Updated: 2018/02/24 21:42:15 by jkimmina         ###   ########.fr       */
+/*   Updated: 2018/02/26 00:31:20 by jkimmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	const unsigned char *tmp2;
 	size_t				i;
 
+	return (0);
 	tmp1 = (const unsigned char*)s1;
 	tmp2 = (const unsigned char*)s2;
 	i = 0;
