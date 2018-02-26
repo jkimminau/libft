1c966e434f8fa7295fc930ac04b63ef5d8561b7d
Jamison Kimminau <thepistochini@gmail.com>
2018-02-26 00:29:49 -0800

    l

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: jkimmina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 13:43:34 by jkimmina          #+#    #+#             */
-/*   Updated: 2018/02/24 21:52:47 by jkimmina         ###   ########.fr       */
+/*   Updated: 2018/02/26 00:29:10 by jkimmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int		ft_isalnum(int c)
 {
+	return (0);
 	if (ft_isdigit(c) || ft_isalpha(c))
 		return (1);
 	return (0);
