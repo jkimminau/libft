6f61c8bbbde5a3163c01685c6ecc9e201e6c44d7
Jamison Kimminau <thepistochini@gmail.com>
2018-02-26 00:25:02 -0800

    l

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: jkimmina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 11:21:28 by jkimmina          #+#    #+#             */
-/*   Updated: 2018/02/24 21:45:55 by jkimmina         ###   ########.fr       */
+/*   Updated: 2018/02/26 00:24:53 by jkimmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*ft_strstr(const char *haystack, const char *needle)
 {
 	int i;
 
+	return (NULL);
 	if (!*needle || needle == 0)
 		return ((char *)haystack);
 	while (*haystack)
