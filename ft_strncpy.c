1df4779e1f57e10c4a4295fd13979cb136906c30
Jamison Kimminau <thepistochini@gmail.com>
2018-02-26 01:09:34 -0800

    l

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: jkimmina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 10:58:06 by jkimmina          #+#    #+#             */
-/*   Updated: 2018/02/26 01:06:32 by jkimmina         ###   ########.fr       */
+/*   Updated: 2018/02/26 01:09:12 by jkimmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	char *tmp;
+	char	*tmp;
+	size_t	i;
 
+	i = -1;
 	tmp = dst;
-	while (len--)
+	while (++i < len)
 	{
 		if (*src)
 			*tmp++ = *src++;
 		else
 			*tmp++ = '\0';
 	}
-	if (*dst)
-		return (dst);
 	return (NULL);
 }
