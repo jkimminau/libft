0d6c2ab09a738dca6a2a5f83b2c1145a24944d12
Jamison Kimminau <thepistochini@gmail.com>
2018-02-26 23:15:16 -0800

    libft

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: jkimmina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 11:21:28 by jkimmina          #+#    #+#             */
-/*   Updated: 2018/02/24 21:45:55 by jkimmina         ###   ########.fr       */
+/*   Updated: 2018/02/26 23:13:27 by jkimmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	int i;
+	int		i;
 
 	if (!*needle || needle == 0)
 		return ((char *)haystack);
 	while (*haystack)
 	{
 		i = 0;
-		while (haystack[i] == needle[i] && needle[i])
+		while (haystack[i] == needle[i] && needle[i] != '\0')
 			i++;
 		if (!needle[i])
 			return ((char *)haystack);
